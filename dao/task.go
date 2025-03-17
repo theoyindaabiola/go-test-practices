8c7cc458f2e759519c467932ce3205d3f115da56
miield <aodasola95@gmail.com>
2025-03-18 00:45:07 +0100

    refactor: update task.go as package class of dao, and communicate with the models

--- dao/task.go
+++ dao/task.go
@@ -2,55 +2,61 @@ package dao
 
 import (
 	"gorm.io/gorm"
+	"taskapi/models"
 )
 
-func CreateTaskDB(task Task) error {
-	if err := db.Create(&task).Error; err != nil {
+type TaskDAO struct {
+	DB *gorm.DB
+}
+
+func NewTaskDAO(db *gorm.DB) *TaskDAO {
+	return &TaskDAO{DB: db}
+}
+
+func (dao *TaskDAO) CreateTaskDB(task models.Task) error {
+	if err := dao.DB.Create(&task).Error; err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func GetTasksDB() ([]Task, error) {
-	var tasks []Task
+func (dao *TaskDAO) GetTasksDB() ([]models.Task, error) {
+	var tasks []models.Task
 	// get all selected from the db and pore them into tasks's memory location
-	if err := db.Find(&tasks).Error; err != nil {
+	if err := dao.DB.Find(&tasks).Error; err != nil {
 		return nil, err
 	}
 	return tasks, nil
 }
 
-func GetTaskDB(id uint) (Task, error) {
-	var task Task
+func (dao *TaskDAO) GetTaskDB(id uint) (models.Task, error) {
+	var task models.Task
 	// get all selected from the db and pore them into tasks's memory location
-	if err := db.Where("id = ?", id).First(&task).Error; err != nil {
+	if err := dao.DB.Where("id = ?", id).First(&task).Error; err != nil {
 		return task, err
 	}
 	return task, nil
 }
 
 // here the GORM accepts map as struct for updating, empty interface is flexible for updating, struct is not.
-func UpdateTaskDB(id uint, task map[string]interface{}) error {
+func (dao *TaskDAO) UpdateTaskDB(id uint, task map[string]interface{}) error {
 	// placeholder for the task to be updated
-	var updateTask Task
+	var updateTask models.Task
 	// finds the task by id and store in the memory location of updateTask
-	if err := db.Where("id = ?", id).First(&updateTask).Error; err != nil {
+	if err := dao.DB.Where("id = ?", id).First(&updateTask).Error; err != nil {
 		return err
 	}
 	// return the fetched task to be updated and update the interface values of task
-	return db.Model(&updateTask).Updates(task).Error
+	return dao.DB.Model(&updateTask).Updates(task).Error
 }
 
-func DeleteTaskDB(id uint) error {
-	var task Task
+func (dao *TaskDAO) DeleteTaskDB(id uint) error {
+	var task models.Task
 	// get all selected from the db and pore them into tasks's memory location
-	if err := db.Where("id = ?", id).First(&task).Error; err != nil {
+	if err := dao.DB.Where("id = ?", id).First(&task).Error; err != nil {
 		return err
 	}
 	// gorm functions have Error
-	return db.Delete(&task).Error
+	return dao.DB.Delete(&task).Error
 }
-
-/// function testing with air...
-/// unit testing...
