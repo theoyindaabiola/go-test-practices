e01495475b21fa779719cc16f7342a88ecfa194c
miield <aodasola95@gmail.com>
2025-03-17 22:35:27 +0100

    create models directory and it's task.go file

---
+++ models/task.go
@@ -0,0 +1,8 @@
+package models
+
+// the struct is used in both JSON and database, thus it is different from the regular struct
+type Task struct {
+	ID 			uint `gorm:"primaryKey" json:"id"`
+	Title 		string `gorm:"not null" json:"title"`
+	Completed 	string `gorm:"default:false" json:"completed"`
+}
